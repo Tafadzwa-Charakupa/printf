00e87586e309287b191578d5fc3b344de9584e11
Tafadzwa-Charakupa <charakupatafah2@gmail.com>
2023-03-25 03:07:04 -0700

    Group Project

--- main.h
+++ main.h
@@ -1,4 +1,3 @@
-
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdarg.h>
@@ -115,4 +114,4 @@ int is_digit(char);
 ilong int convert_size_number(long int num, int size);
 long int convert_size_unsgnd(unsigned long int num, int size);
 
-#endif /* MAIN_H */
+#endif
