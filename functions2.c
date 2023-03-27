a0a2b594f746ee50dc5f2dd5988350cbe40dc418
Nothando13 <123740766+Nothando13@users.noreply.github.com>
2023-03-27 14:10:07 +0200

    Update functions2.c

--- functions2.c
+++ functions2.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /****************** PRINT POINTER ******************/
 /**
  * print_pointer - Prints the value of a pointer variable
@@ -51,7 +50,6 @@ int print_pointer(va_list types, char buffer[],
 	return (write_pointer(buffer, ind, length,
 		width, flags, padd, extra_c, padd_start));
 }
-
 /************************* PRINT NON PRINTABLE *************************/
 /**
  * print_non_printable - Prints ascii codes in hexa of non printable chars
